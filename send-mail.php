4a65b9a37c160d6652a621e5b03cdf15a1543f6d
AnnaBorisenko <anutka.kiska.87@gmail.com>
2021-09-04 08:11:10 +0300

    pull changes

--- send-mail.php
+++ send-mail.php
@@ -90,9 +90,10 @@ $message = 'Ваша заявка прийнята ' . "\r\n" . ' ми з вам
 
 mail($to, $subject, $message, $headers);
 
+$boundary = "---";
 $headers = array(
     'MIME-Version: 1.0' . "\r\n",
-    'Content-type: text/html; charset=iso-8859-1' . "\r\n",
+    "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n",
     'From' => 'webmaster@example.com',
     'Reply-To' => 'webmaster@example.com',
     'X-Mailer' => 'PHP/' . phpversion()
@@ -142,6 +143,23 @@ $message = "
 </body>
 </html>
 ";
+$filename = "form.txt";
+$body = "--$boundary\n";
+/* Присоединяем текстовое сообщение */
+$body .= "Content-type: text/html; charset='utf-8'\n";
+$body .= "Content-Transfer-Encoding: quoted-printablenn";
+$body .= "Content-Disposition: attachment; filename==?utf-8?B?".base64_encode($filename)."?=\n\n";
+$body .= $message."\n";
+$body .= "--$boundary\n";
+$file = fopen($filename, "r"); //Открываем файл
+$text = fread($file, filesize($filename)); //Считываем весь файл
+fclose($file); //Закрываем файл
+/* Добавляем тип содержимого, кодируем текст файла и добавляем в тело письма */
+$body .= "Content-Type: application/octet-stream; name==?utf-8?B?".base64_encode($filename)."?=\n";
+$body .= "Content-Transfer-Encoding: base64\n";
+$body .= "Content-Disposition: attachment; filename==?utf-8?B?".base64_encode($filename)."?=\n\n";
+$body .= chunk_split(base64_encode($text))."\n";
+$body .= "--".$boundary ."--\n";
+
 mail($to, $subject, $message, $headers);
-
-
+header('Location: /');
